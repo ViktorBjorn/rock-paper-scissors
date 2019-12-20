0e7c512451e7c97e8f811b0d361cb67878fb7b61
Viktor Bjorn <viktorbjorn85@gmail.com>
2019-12-20 16:48:09 +0000

    game script working with ui

--- js/game.js
+++ js/game.js
@@ -3,6 +3,21 @@ let computerScore = 0;
 let playerScore = 0;
 const userScore_p = document.getElementById("user-score");
 const computerScore_p = document.getElementById("computer-score");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+const result_div = document.querySelector(".result");
+
+
+rock_div.addEventListener('click', function() {
+    game("rock");
+});
+paper_div.addEventListener('click', function() {
+    game("paper");
+});
+scissors_div.addEventListener('click', function() {
+    game("scissors");
+});
 
 function computerPlay() {
     let randomNum = Math.floor(Math.random() * 3);
@@ -13,51 +28,38 @@ function computerPlay() {
 function win() {
     playerScore++;
     userScore_p.innerHTML = playerScore;
-    computerScore_p.innerHTML = computerScore;
 }
 
 function lose() {
     computerScore++;
     computerScore_p.innerHTML = computerScore;
-    userScore_p.innerHTML = playerScore;
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'rock' && computerSelection == 'Scissors') {
         win();
-        return `You Win! Rock beats Scissors. Score: ${playerScore} to ${computerScore}`;
+        result_div.innerHTML = `You Win! Rock beats Scissors`;
     } else if (playerSelection == 'rock' && computerSelection == 'Paper') {
         lose();
-        return `You Lose! Paper covers rock. Score: ${playerScore} to ${computerScore}`;
+        result_div.innerHTML = `You Lose! Paper covers rock`;
     } else if (playerSelection == 'paper' && computerSelection == 'Scissors') {
         lose();
-        return `You Lose! Scissors cut paper. Score: ${playerScore} to ${computerScore}`; 
+        result_div.innerHTML = `You Lose! Scissors cut paper`; 
     } else if (playerSelection == 'paper' && computerSelection == 'Rock') {
         win();
-        return `You Win! Paper covers rock. Score: ${playerScore} to ${computerScore}`;
+        result_div.innerHTML = `You Win! Paper covers rock`;
     } else if (playerSelection == 'scissors' && computerSelection == "Rock") {
         lose();
-        return `You Lose! Rocks beats Scissors. Score: ${playerScore} to ${computerScore}`;
+        result_div.innerHTML = `You Lose! Rocks beats Scissors`;
     } else if (playerSelection == 'scissors' && computerSelection == 'Paper') {
         win();
-        return `You Win! Scissors beat paper. Score: ${playerScore} to ${computerScore}`;
+        result_div.innerHTML = `You Win! Scissors beat paper`;
     }
-    else return `That was a draw. Try Again! Score: ${playerScore} to ${computerScore}`;
+    else result_div.innerHTML = `That was a draw. Try Again!`;
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Type: Rock, Paper, or Scissors')
-        playerSelection = playerSelection.toLowerCase();
+function game(playerSelection) {
         const computerSelection = computerPlay();
-        console.log(computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-    }
-    if (playerScore > computerScore) {
-        console.log(`You're the winner! Final Score: ${playerScore} to ${computerScore}`);
-    } else if (computerScore > playerScore) {
-        console.log(`You're the loser. Final Score: ${playerScore} to ${computerScore}`);
-    } else
-        console.log(`You tied. Final Score: ${playerScore} to ${computerScore}`);
+        playRound(playerSelection, computerSelection);  
 }
 game();
